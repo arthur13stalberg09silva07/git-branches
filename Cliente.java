ff78a85d6b25604e08dae0d3d541377f0df87e9d
Arthur Stalberg Siva <arthur.stalberg@germinare.org.br>
2023-03-15 19:21:07 -0300

    corrigindo o programa.java

--- Cliente.java
+++ Cliente.java
@@ -1,10 +1,7 @@
 public class Cliente {
     private String nome;
-<<<<<<< HEAD
     private String cpf_Stalberg;
-=======
     private String cpf_maju;
->>>>>>> origin/classe-cliente
     private int agencia;
     private double saldo;
     
